67582b9e40cf0a018af67fa3e0ca330dde838c6d
Hurera sheikh <152999019+Abuhurera00@users.noreply.github.com>
2024-01-17 10:00:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+let string="";
+let buttons = document.querySelectorAll('.button');
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+        string = eval(string);
+        document.querySelector('input').value = string;
+    }
+
+    if(e.target.innerHTML == 'C'){
+        string = "";
+        document.querySelector('input').value = string;
+    }
+
+  else{
+       string = string + e.target.innerHTML;
+       document.querySelector('input').value = string;
+  }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let string = "";
+// let buttons = document.querySelectorAll('.button');
+// Array.from(buttons).forEach((button)=>{
+//     button.addEventListener('click', (e)=>{
+//     if(e.target.innerHTML == '='){
+//         string = eval(string);
+//         document.querySelector('input').value = string;
+//     }
+//     if(e.target.innerHTML == 'C'){
+//         string = "";
+//         document.querySelector('input').value = string;
+//     }
+//     else{
+//         console.log(e.target)
+//         string = string + e.target.innerHTML;
+//         document.querySelector('input').value = string;
+//     }
+//     })
+// })
+
+
+
+
+// let string = "";
+// let buttons = document.querySelectorAll('.buttons');
+
+// Array.from(buttons).forEach((button) => {
+//     button.addEventListener('click', (e) => {
+//         if (e.target.innerHTML === '=') {
+//             string = eval(string);
+//             document.querySelector("input").value = string;
+//         } else {
+//             console.log(e.target)
+//             string = string + e.target.innerHTML;
+//             document.querySelector('input').value = string;
+//         }
+//     });
+// });
